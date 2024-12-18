d070a8ba51675945b982b01529f93e931740f9f3
Ads Dawson <104169244+GangGreenTemperTatum@users.noreply.github.com>
2024-12-18 16:37:13 -0500

    chore: rm dups

--- cuda_tracer/src/__init__.py
+++
@@ -1,4 +0,0 @@
-from .tracer import CUDATracer
-from .categories import CUDA_CATEGORIES
-
-__all__ = ['CUDATracer', 'CUDA_CATEGORIES']
--- cuda_tracer/src/categories.py
+++
@@ -1,8 +0,0 @@
-CUDA_CATEGORIES = {
-    'memory': ['cuMemAlloc', 'cuMemFree', 'cuMemcpy'],
-    'execution': ['cuLaunchKernel', 'cuStreamSynchronize'],
-    'context': ['cuCtxCreate', 'cuCtxDestroy', 'cuCtxPushCurrent'],
-    'stream': ['cuStreamCreate', 'cuStreamDestroy', 'cuStreamWaitEvent'],
-    'event': ['cuEventCreate', 'cuEventRecord', 'cuEventSynchronize'],
-    'module': ['cuModuleLoad', 'cuModuleGetFunction', 'cuModuleUnload']
-}
